6fd44a6596e1bc255d116732d1087f0273478b9c
Tarek <tarek.shehabeldin.ibrahim@gmail.com>
2022-08-25 02:59:09 +0200

    Add necesary Router, Routes, and Route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component, StrictMode } from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks.js'
 import BookShelf from './BookShelf'
-import { Link } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
+
 
 class BooksApp extends Component {
   state = {
-    // state variable to keep track of which page we're on => use URL in address bar.
     books: [],
     searchResults: [],
-    showSearchPage: false
-  }
-
-  closeSearchWindow = () => {
-    this.setState({ showSearchPage: false })
-  }
-
-  openSearchWindow = () => {
-    this.setState({ showSearchPage: true })
   }
 
   // Fetch books from server and update books in state
@@ -27,14 +18,15 @@ class BooksApp extends Component {
       .then(res => this.setState({books: res}))
   }
 
+  // Change the shelf of a book in the server 
   handleChangeShelf = async (bookId, newShelf) => {
-    console.log('changing shelf...', this.state.books)
     await BooksAPI.update(bookId, newShelf)
       .then(this.getBooks())
   }
 
-  // Still not working
+  // Fetch query results from server and edit them and update searchResults in the state
   handlSearchQuery = async (query) => {
+    // Handle the case if the user removes his input from search box
     if(query===''){
       this.setState({searchResults: []})
       return
@@ -42,16 +34,17 @@ class BooksApp extends Component {
     try{
       await BooksAPI.search(query)
         .then(res => {
-          // Handle empty response
+          // Handle empty response (no matches with query)
           if(res.error){
             this.setState({searchResults: []})
             return
           }
           const results = res.map(book => {
-            // Not found in state's books
+            // Not in state's books => none
             if (this.state.books.find(b => b.id === book.id) === undefined){
               book.shelf = 'none'
             }
+            // in state's books => the shelf it's in
             else{
               const theBook = this.state.books.find(b => b.id === book.id)
               const shelfName = theBook.shelf
@@ -59,16 +52,15 @@ class BooksApp extends Component {
             }
             return book
           })
-          // console.log(results)
           this.setState({searchResults: results})
         })
     }
     catch(error){
           console.log('Invalid query my friend', error)
-          this.setState({searchResults: []})
     }
 
   }
+  
   componentDidMount = () => {
     this.getBooks()
   }
@@ -76,53 +68,57 @@ class BooksApp extends Component {
   render() {
 
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks
-            onCloseSearch = {this.closeSearchWindow}
-            searchResults = {this.state.searchResults}
-            onInputChange = {this.handlSearchQuery}
-            onchangeShelf = {this.handleChangeShelf}
-          />
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              {/* shelfs */}
-              <div>
-                <BookShelf
-                  shelfTitle = 'Currently Reading'
-                  books = {this.state.books}
-                  onchangeShelf = {this.handleChangeShelf}
-                />
+      <StrictMode>
+        <div className="app">
+          <Routes>
+            <Route exact path='/' render={() => (
+              <div className="list-books">
+                <div className="list-books-title">
+                  <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                  {/* shelfs */}
+                  <div>
+                    <BookShelf
+                      shelfTitle = 'Currently Reading'
+                      books = {this.state.books}
+                      onchangeShelf = {this.handleChangeShelf}
+                    />
 
-                <BookShelf
-                  shelfTitle = 'Want to Read'
-                  books = {this.state.books}
-                  onchangeShelf = {this.handleChangeShelf}
-                />
+                    <BookShelf
+                      shelfTitle = 'Want to Read'
+                      books = {this.state.books}
+                      onchangeShelf = {this.handleChangeShelf}
+                    />
 
-                <BookShelf
-                  shelfTitle = 'Read'
-                  books = {this.state.books}
-                  onchangeShelf = {this.handleChangeShelf}
-                />
-              </div>
-            </div>
-            {/* Search button */}
-            <div className="open-search">
-              <Link 
-                to='/search'
-                onClick={() => this.openSearchWindow()}
-              >
-                Add a book
-              </Link>
-            </div>
-          </div>
-        )}
-      </div>
+                    <BookShelf
+                      shelfTitle = 'Read'
+                      books = {this.state.books}
+                      onchangeShelf = {this.handleChangeShelf}
+                    />
+                  </div>
+                </div>
+                {/* Search link */}
+                <div className="open-search">
+                  <Link 
+                    to='/search'
+                  >
+                    Add a book
+                  </Link>
+                </div>
+              </div>)}
+            />
+            
+            <Route path='/search' render={() => (
+              <SearchBooks
+                searchResults = {this.state.searchResults}
+                onInputChange = {this.handlSearchQuery}
+                onchangeShelf = {this.handleChangeShelf}
+              />)}
+            />
+          </Routes>   
+        </div>
+      </StrictMode>  
     )
   }
 }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,25 +1,22 @@
 import React, { Component} from 'react'
 import Book from './Book';
+import { Link } from 'react-router-dom'
 
 class SearchBooks extends Component {
 
     render() {
 
-        const {onCloseSearch, searchResults, onInputChange, onchangeShelf} = this.props;
+        const {searchResults, onInputChange, onchangeShelf} = this.props;
 
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <button className="close-search" onClick={() => onCloseSearch()}>Close</button>
+                    <Link 
+                        to='/'
+                        className="close-search" 
+                    >   Close
+                    </Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                        */}
                         <input 
                             type="text" 
                             placeholder="Search by title or author"
